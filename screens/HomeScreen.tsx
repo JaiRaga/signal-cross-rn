0ed81d495115c8e5323121fd6f03cfb78aab187a
JaiRaga <raga.j.santhosh@gmail.com>
2022-04-13 11:00:15 +0530

    :zap: changed the screen name to HomeScreen

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,30 @@
+import { View, Image, Text, StyleSheet, FlatList } from "react-native";
+import ChatRoomItem from "../components/ChatRoomItem";
+import { RootTabScreenProps } from "../types";
+
+import chatRooms from "../assets/dummy-data/ChatRooms";
+
+const chatRoom1 = chatRooms[3];
+const chatRoom2 = chatRooms[1];
+
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  return (
+    <View style={styles.page}>
+      <FlatList
+        data={chatRooms}
+        renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
+        showsVerticalScrollIndicator={false}
+        // onEndReached={() => console.log("List Ended...")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: "white",
+    flex: 1,
+  },
+});

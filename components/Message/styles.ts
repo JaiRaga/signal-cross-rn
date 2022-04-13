fe8ebc4256b2b3b18644629e20723f4f2c86141b
JaiRaga <raga.j.santhosh@gmail.com>
2022-04-13 11:01:25 +0530

    :sparkles: contains layout and styles for users messages

---
+++ components/Message/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+    maxWidth: "75%"
+  },
+  rightContainer: {
+    backgroundColor: '#3777f0',
+    marginLeft: "auto",
+    marginRight: 10
+  },
+  leftContainer: {
+    backgroundColor: 'lightgray',
+    marginLeft: 10,
+    marginRight: "auto"
+  }
+})
+
+
+export default styles

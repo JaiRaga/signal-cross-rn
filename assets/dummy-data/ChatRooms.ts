788d49821fbc51c6c4ac07b10a9ef988aa3c922f
JaiRaga <raga.j.santhosh@gmail.com>
2022-04-15 06:35:51 +0530

    :package: updated

---
+++ assets/dummy-data/ChatRooms.ts
@@ -0,0 +1,445 @@
+export default [
+	{
+		id: '1',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u2',
+				name: 'Elon Musk',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
+			},
+		],
+		lastMessage: {
+			id: 'm1',
+			content: 'btw, SpaceX is interested in buying postX',
+			createdAt: '2020-10-03T14:48:00.000Z',
+		},
+		newMessages: 17,
+	},
+	{
+		id: '2',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u3',
+				name: 'Jeff',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg',
+			},
+		],
+		lastMessage: {
+			id: 'm2',
+			content: 'Why did you reject our offer?',
+			createdAt: '2020-10-02T15:40:00.000Z',
+		},
+	},
+	{
+		id: '3',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u4',
+				name: 'Zuck',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg',
+			},
+		],
+		lastMessage: {
+			id: 'm3',
+			content: 'Is signal really better than my Whatsapp?',
+			createdAt: '2020-10-02T14:48:00.000Z',
+		},
+	},
+	{
+		id: '4',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u5',
+				name: 'Graham',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/graham.jpg',
+			},
+		],
+		lastMessage: {
+			id: 'm4',
+			content: 'Destroy the like button!',
+			createdAt: '2020-09-29T14:48:00.000Z',
+		},
+	},
+	{
+		id: '5',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u6',
+				name: 'Biahaze',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/biahaze.jpg',
+			},
+		],
+		lastMessage: {
+			id: 'm5',
+			content: 'I would be happy',
+			createdAt: '2020-09-30T14:48:00.000Z',
+		},
+	},
+	{
+		id: '6',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u7',
+				name: 'Sus?',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
+			},
+		],
+		lastMessage: {
+			id: 'm6',
+			content: 'Who sus?',
+			createdAt: '2020-10-02T15:40:00.000Z',
+		},
+	},
+	{
+		id: '7',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u8',
+				name: 'Daniel',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/2.jpg',
+			},
+		],
+		lastMessage: {
+			id: 'm7',
+			content: 'How are you doing?',
+			createdAt: '2020-10-02T15:40:00.000Z',
+		},
+	},
+	{
+		id: '8',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u9',
+				name: 'Carlos',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/3.jpg',
+			},
+		],
+		lastMessage: {
+			id: 'm8',
+			content: 'Hola hola coca cola?',
+			createdAt: '2020-09-27T15:40:00.000Z',
+		},
+	},
+	{
+		id: '9',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u10',
+				name: 'Angelina Jolie',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/4.jpg',
+			},
+		],
+		lastMessage: {
+			id: 'm9',
+			content: 'Meet me at the same place',
+			createdAt: '2020-09-25T15:40:00.000Z',
+		},
+	},
+	{
+		id: '10',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u2',
+				name: 'Elon Musk',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
+			},
+		],
+		lastMessage: {
+			id: 'm1',
+			content: 'btw, SpaceX is interested in buying postX',
+			createdAt: '2020-10-03T14:48:00.000Z',
+		},
+		newMessages: 17,
+	},
+	{
+		id: '11',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u3',
+				name: 'Jeff',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg',
+			},
+		],
+		lastMessage: {
+			id: 'm2',
+			content: 'Why did you reject our offer?',
+			createdAt: '2020-10-02T15:40:00.000Z',
+		},
+	},{
+		id: '12',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u2',
+				name: 'Elon Musk',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
+			},
+		],
+		lastMessage: {
+			id: 'm1',
+			content: 'btw, SpaceX is interested in buying postX',
+			createdAt: '2020-10-03T14:48:00.000Z',
+		},
+		newMessages: 17,
+	},
+	{
+		id: '13',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u3',
+				name: 'Jeff',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg',
+			},
+		],
+		lastMessage: {
+			id: 'm2',
+			content: 'Why did you reject our offer?',
+			createdAt: '2020-10-02T15:40:00.000Z',
+		},
+	},{
+		id: '14',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u2',
+				name: 'Elon Musk',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
+			},
+		],
+		lastMessage: {
+			id: 'm1',
+			content: 'btw, SpaceX is interested in buying postX',
+			createdAt: '2020-10-03T14:48:00.000Z',
+		},
+		newMessages: 17,
+	},
+	{
+		id: '15',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u3',
+				name: 'Jeff',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg',
+			},
+		],
+		lastMessage: {
+			id: 'm2',
+			content: 'Why did you reject our offer?',
+			createdAt: '2020-10-02T15:40:00.000Z',
+		},
+	},{
+		id: '16',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u2',
+				name: 'Elon Musk',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
+			},
+		],
+		lastMessage: {
+			id: 'm1',
+			content: 'btw, SpaceX is interested in buying postX',
+			createdAt: '2020-10-03T14:48:00.000Z',
+		},
+		newMessages: 17,
+	},
+	{
+		id: '17',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u3',
+				name: 'Jeff',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg',
+			},
+		],
+		lastMessage: {
+			id: 'm2',
+			content: 'Why did you reject our offer?',
+			createdAt: '2020-10-02T15:40:00.000Z',
+		},
+	},{
+		id: '18',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u2',
+				name: 'Elon Musk',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
+			},
+		],
+		lastMessage: {
+			id: 'm1',
+			content: 'btw, SpaceX is interested in buying postX',
+			createdAt: '2020-10-03T14:48:00.000Z',
+		},
+		newMessages: 17,
+	},
+	{
+		id: '19',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u3',
+				name: 'Jeff',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg',
+			},
+		],
+		lastMessage: {
+			id: 'm2',
+			content: 'Why did you reject our offer?',
+			createdAt: '2020-10-02T15:40:00.000Z',
+		},
+	},{
+		id: '20',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u2',
+				name: 'Elon Musk',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
+			},
+		],
+		lastMessage: {
+			id: 'm1',
+			content: 'btw, SpaceX is interested in buying postX',
+			createdAt: '2020-10-03T14:48:00.000Z',
+		},
+		newMessages: 17,
+	},
+	{
+		id: '21',
+		users: [
+			{
+				id: 'u1',
+				name: 'Raga',
+				imageUri: 'https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4',
+			},
+			{
+				id: 'u3',
+				name: 'Jeff',
+				imageUri:
+					'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg',
+			},
+		],
+		lastMessage: {
+			id: 'm2',
+			content: 'Why did you reject our offer?',
+			createdAt: '2020-10-02T15:40:00.000Z',
+		},
+	},
+]

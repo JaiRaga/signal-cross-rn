92074800aac9acea24b8b48c8503af8e2d5796ab
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-26 18:58:28 +0530

    :sparkles: added styling for header of chat room screen and added navigation to go back

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,36 +3,34 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { FontAwesome, Feather } from "@expo/vector-icons";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome, Feather } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {
   NavigationContainer,
   DefaultTheme,
   DarkTheme,
-} from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import * as React from "react";
-import {
-  ColorSchemeName,
-  View,
-  Image,
-  Text,
-  useWindowDimensions,
-} from "react-native";
+  useNavigation,
+} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HeaderBackButton } from '@react-navigation/elements';
+import * as React from 'react';
+import { ColorSchemeName } from 'react-native';
 
-import Colors from "../constants/Colors";
-import useColorScheme from "../hooks/useColorScheme";
-import ModalScreen from "../screens/ModalScreen";
-import NotFoundScreen from "../screens/NotFoundScreen";
-import HomeScreen from "../screens/HomeScreen";
-import ChatRoomScreen from "../screens/ChatRoomScreen";
-import TabTwoScreen from "../screens/TabTwoScreen";
+import Colors from '../constants/Colors';
+import useColorScheme from '../hooks/useColorScheme';
+import ModalScreen from '../screens/ModalScreen';
+import NotFoundScreen from '../screens/NotFoundScreen';
+import HomeScreen from '../screens/HomeScreen';
+import ChatRoomScreen from '../screens/ChatRoomScreen';
+import TabTwoScreen from '../screens/TabTwoScreen';
 import {
   RootStackParamList,
   RootTabParamList,
   RootTabScreenProps,
-} from "../types";
-import LinkingConfiguration from "./LinkingConfiguration";
+} from '../types';
+import LinkingConfiguration from './LinkingConfiguration';
+import HomeHeader from '../components/HomeHeader';
+import ChatRoomHeader from '../components/ChatRoomHeader';
 
 export default function Navigation({
   colorScheme,
@@ -42,7 +40,7 @@ export default function Navigation({
   return (
     <NavigationContainer
       linking={LinkingConfiguration}
-      theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
     >
       <RootNavigator />
     </NavigationContainer>
@@ -56,6 +54,7 @@ export default function Navigation({
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
+  const navigation = useNavigation();
   return (
     <Stack.Navigator>
       <Stack.Screen
@@ -63,49 +62,36 @@ function RootNavigator() {
         component={HomeScreen}
         options={{ headerTitle: HomeHeader }}
       />
-      <Stack.Screen name="ChatRoom" component={ChatRoomScreen} />
+      <Stack.Screen
+        name="ChatRoom"
+        component={ChatRoomScreen}
+        options={{
+          headerBackVisible: false,
+          headerBackTitleVisible: false,
+          headerTitle: ChatRoomHeader,
+          headerLeft: (props) => (
+            <HeaderBackButton
+              {...props}
+              style={{ marginRight: 0 }}
+              onPress={() => navigation.goBack()}
+            />
+          ),
+        }}
+      />
       <Stack.Screen
         name="NotFound"
         component={NotFoundScreen}
-        options={{ title: "Oops!" }}
+        options={{ title: 'Oops!' }}
       />
     </Stack.Navigator>
   );
 }
 
-const HomeHeader = () => {
-  const { width } = useWindowDimensions();
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        width,
-        padding: 0,
-        backgroundColor: "blue",
-      }}
-    >
-      <Image
-        source={{
-          uri: "https://avatars.githubusercontent.com/u/44367062?s=400&u=8d55969ede5e1034fe015603d80b82958d249392&v=4",
-        }}
-        style={{ width: 30, height: 30, borderRadius: 30 }}
-      />
-      <Text style={{}}>Home</Text>
-      <View style={{flexDirection: "row"}}>
-        <Feather name="camera" size={24} color="black" />
-        <Feather name="edit-2" size={24} color="black" />
-      </View>
-    </View>
-  );
-};
-
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>["name"];
+  name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
 }) {
   return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;

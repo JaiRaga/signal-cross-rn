7d2e3d7057a7fd5fdfb4a9a937f76394ed8ac454
JaiRaga <raga.j.santhosh@gmail.com>
2022-06-05 19:17:55 +0530

    :sparkles: created relationship between chatroom and users

--- components/UserItem/UserItem.tsx
+++ components/UserItem/UserItem.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import { View, Image, Text, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Auth, DataStore } from 'aws-amplify';
+import { User, ChatRoom, ChatRoomUser } from '../../src/models';
+
 import styles from './styles';
 
 const UserItem = ({ user }) => {
   const navigation = useNavigation();
 
-  const onPress = () => {
+  const onPress = async () => {
+    // Check for existing chat room between two users
+    
+
     // create a chat room
+    const newChatRoom = await DataStore.save(new ChatRoom({ newMessages: 0 }));
+
+    // Connect authenticated user with the chat room
+    const authUser = await Auth.currentAuthenticatedUser();
+    const dbUser = await DataStore.query(User, authUser.attributes.sub);
+    await DataStore.save(
+      new ChatRoomUser({
+        user: dbUser,
+        chatRoom: newChatRoom,
+      })
+    );
+
+    // Connect clicked user with the chat room
+    await DataStore.save(
+      new ChatRoomUser({
+        user,
+        chatRoom: newChatRoom,
+      })
+    );
+
+    // Navigate to the new chat room
+    navigation.navigate('ChatRoom', { id: newChatRoom.id });
   };
 
   return (

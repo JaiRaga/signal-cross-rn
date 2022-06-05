dd00043e244cd6ed7f044eaf2d95c45b06776e25
JaiRaga <raga.j.santhosh@gmail.com>
2022-06-05 19:19:20 +0530

    :zap: added chat room type

--- types.tsx
+++ types.tsx
@@ -3,12 +3,12 @@
  * https://reactnavigation.org/docs/typescript/
  */
 
-import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import {
   CompositeScreenProps,
   NavigatorScreenParams,
-} from "@react-navigation/native";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
+} from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 declare global {
   namespace ReactNavigation {
@@ -30,6 +30,7 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> =
 
 export type RootTabParamList = {
   Home: undefined;
+  ChatRoom: undefined;
   TabTwo: undefined;
 };
 

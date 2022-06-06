0f1ee49f6a1fc6f84da3a0ce9dd41495640457a4
JaiRaga <raga.j.santhosh@gmail.com>
2022-06-06 19:19:17 +0530

    :zap: ignored errors

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import { LogBox } from 'react-native';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import Amplify, {Auth} from 'aws-amplify';
+import Amplify, { Auth } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import config from './src/aws-exports';
+
+LogBox.ignoreLogs([
+  'Setting a timer',
+  'Unhandled promise rejection: Error: No credentials, applicationId or region',
+]);
 
 Amplify.configure(config);
 

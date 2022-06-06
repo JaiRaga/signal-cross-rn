66ab52c0fc3555c7da5496473ca4e75fd4688153
JaiRaga <raga.j.santhosh@gmail.com>
2022-06-06 19:16:45 +0530

    :zap: added logic to differentiate the message sent by the auth user and the rest

--- components/Message/Message.tsx
+++ components/Message/Message.tsx
@@ -1,17 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import styles from './styles'
+import { View, Text, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Auth, DataStore } from 'aws-amplify';
+import { User } from '../../src/models';
 
-const myId = "u1"
+import styles from './styles';
 
-const Message = ({message}) => {
-  const isMe = message.user.id === myId
-  
+const Message = ({ message }) => {
+  const [user, setUser] = useState<User | undefined>();
+  const [isMe, setIsMe] = useState<boolean>(false);
+
+  useEffect(() => {
+    DataStore.query(User, message.userID).then(setUser);
+  }, []);
+
+  useEffect(() => {
+    const checkIfMe = async () => {
+      if (!user) {
+        return;
+      }
+
+      const authUser = await Auth.currentAuthenticatedUser();
+      setIsMe(user.id === authUser.attributes.sub);
+    };
+
+    checkIfMe();
+  }, [user]);
+
+  if (!user) {
+    return <ActivityIndicator color="green" size="small" />;
+  }
+
   return (
-    <View style={[styles.container, isMe ? styles.rightContainer : styles.leftContainer]}>
-      <Text style={{color: isMe ? "white" : "black"}}>{message.content}</Text>
+    <View
+      style={[
+        styles.container,
+        isMe ? styles.rightContainer : styles.leftContainer,
+      ]}
+    >
+      <Text style={{ color: isMe ? 'white' : 'black' }}>{message.content}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;

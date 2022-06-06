4a75f8cdbf30da7835b0ae91a47e753bf96ec6e8
JaiRaga <raga.j.santhosh@gmail.com>
2022-06-06 19:18:51 +0530

    :sparkles: queried datastore to get chatroom messages and added subscriptions to get real time messages

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,25 +1,86 @@
-import { View, Text, StyleSheet, FlatList, SafeAreaView } from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  SafeAreaView,
+  ActivityIndicator,
+} from 'react-native';
+import React, { useState, useEffect } from 'react';
 import { useRoute, useNavigation } from '@react-navigation/native';
+import { DataStore, SortDirection } from 'aws-amplify';
+import { Message as MessageModel, ChatRoom } from '../src/models';
 import Message from '../components/Message';
-import chatRoomData from '../assets/dummy-data/Chats';
 import MessageInput from '../components/MessageInput';
 import { RootTabScreenProps } from '../types';
 
 const ChatRoomScreen = () => {
-  // const route = useRoute()
+  const [messages, setMessages] = useState<MessageModel[]>([]);
+  const [chatRoom, setChatRoom] = useState<ChatRoom | null>(null);
+
+  const route = useRoute();
   const navigation = useNavigation();
 
-  navigation.setOptions({ title: 'user' });
+  useEffect(() => {
+    fetchChatRoom();
+  }, []);
+
+  useEffect(() => {
+    fetchMessages();
+  }, [chatRoom]);
+
+  useEffect(() => {
+    const subscription = DataStore.observe(MessageModel).subscribe((msg) => {
+      // console.log(msg.model, msg.opType, msg.element);
+      if (msg.model === MessageModel && msg.opType === 'INSERT') {
+        setMessages((existingMessage) => [msg.element, ...existingMessage]);
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  const fetchChatRoom = async () => {
+    if (!route.params?.id) {
+      console.warn('No chatRoom id provided');
+      return;
+    }
+    const chatRoom = await DataStore.query(ChatRoom, route.params.id);
+    if (!chatRoom) {
+      console.log("Couldn't find a chat room aith the id");
+    } else {
+      setChatRoom(chatRoom);
+    }
+  };
+
+  const fetchMessages = async () => {
+    if (!chatRoom) {
+      return;
+    }
+    const fetchedMessages = await DataStore.query(
+      MessageModel,
+      (message) => message.chatroomID('eq', chatRoom?.id),
+      {
+        sort: (message) => message.createdAt(SortDirection.DESCENDING),
+      }
+    );
+    setMessages(fetchedMessages);
+  };
+
+  // navigation.setOptions({ title: 'user' });
+
+  if (!chatRoom) {
+    return <ActivityIndicator color="gold" size="large" />;
+  }
 
   return (
     <SafeAreaView style={styles.page}>
       <FlatList
-        data={chatRoomData.messages}
+        data={messages}
         renderItem={({ item }) => <Message message={item} />}
         inverted
       />
-      <MessageInput />
+      <MessageInput chatRoom={chatRoom} />
     </SafeAreaView>
   );
 };

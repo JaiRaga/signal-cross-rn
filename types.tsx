d08a3f75cc89de3d45bb8a1bedfeb8be7af6479d
JaiRaga <44367062+JaiRaga@users.noreply.github.com>
2022-07-14 18:48:34 +0530

    :zap: changed chatRoom params type

--- types.tsx
+++ types.tsx
@@ -20,7 +20,7 @@ export type RootStackParamList = {
   Root: NavigatorScreenParams<RootTabParamList> | undefined;
   Modal: undefined;
   NotFound: undefined;
-  ChatRoom: undefined;
+  ChatRoom: {id: string};
   Home: undefined;
   UsersScreen: undefined;
 };
